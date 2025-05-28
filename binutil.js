73fb4c7c9cf9f056f41c7e6a8692dfa797d13a24
Taisuke Fukuno <fukuno@jig.jp>
2025-05-29 07:29:37 +0900

    added little endian for int and short

--- binutil.js
+++ binutil.js
@@ -5,11 +5,19 @@ export const bin2s = (bin, n, len) => {
   }
   return new TextDecoder().decode(b);
 };
-export const bin2i = (bin, n) => {
-  return ((bin[n] & 0xff) << 24) | ((bin[n + 1] & 0xff) << 16) | ((bin[n + 2] & 0xff) << 8) | (bin[n + 3] & 0xff);
+export const bin2i = (bin, n, littleendian) => { // default bigendian
+  if (littleendian) {
+    return ((bin[n + 3] & 0xff) << 24) | ((bin[n + 2] & 0xff) << 16) | ((bin[n + 1] & 0xff) << 8) | (bin[n] & 0xff);
+  } else {
+    return ((bin[n] & 0xff) << 24) | ((bin[n + 1] & 0xff) << 16) | ((bin[n + 2] & 0xff) << 8) | (bin[n + 3] & 0xff);
+  }
 };
-export const bin2short = (bin, n) => {
-  return ((bin[n] & 0xff) << 8) | (bin[n + 1] & 0xff);
+export const bin2short = (bin, n, littleendian) => { // bigendian
+  if (littleendian) {
+    return ((bin[n + 1] & 0xff) << 8) | (bin[n] & 0xff);
+  } else {
+    return ((bin[n] & 0xff) << 8) | (bin[n + 1] & 0xff);
+  }
 };
 export const subbin = (bin, n, len) => {
   if (len === undefined) {
@@ -27,15 +35,27 @@ export const setbin = (bin, off, b) => {
   }
   return bin;
 };
-export const i2bin = (b, off, n) => {
-  b[off] = (n >> 24) & 0xff;
-  b[off + 1] = (n >> 16) & 0xff;
-  b[off + 2] = (n >> 8) & 0xff;
-  b[off + 3] = n & 0xff;
+export const i2bin = (b, off, n, littleendian) => { // big endian
+  if (littleendian) {
+    b[off + 3] = (n >> 24) & 0xff;
+    b[off + 2] = (n >> 16) & 0xff;
+    b[off + 1] = (n >> 8) & 0xff;
+    b[off] = n & 0xff;
+  } else {
+    b[off] = (n >> 24) & 0xff;
+    b[off + 1] = (n >> 16) & 0xff;
+    b[off + 2] = (n >> 8) & 0xff;
+    b[off + 3] = n & 0xff;
+  }
 };
-export const short2bin = (b, off, n) => {
-  b[off] = (n >> 8) & 0xff;
-  b[off + 1] = n & 0xff;
+export const short2bin = (b, off, n, littleendian) => { // big endian
+  if (littleendian) {
+    b[off + 1] = (n >> 8) & 0xff;
+    b[off] = n & 0xff;
+  } else {
+    b[off] = (n >> 8) & 0xff;
+    b[off + 1] = n & 0xff;
+  }
 };
 export const s2bin = (bin, off, s) => {
   setbin(bin, off, new TextEncoder().encode(s));
--- binutil.test.js
+++ binutil.test.js
@@ -1,11 +1,28 @@
 import * as t from "https://deno.land/std/testing/asserts.ts";
-import { bin2short, short2bin } from "./binutil.js";
+import { bin2short, short2bin, i2bin, bin2i } from "./binutil.js";
 
-Deno.test("short", () => {
+Deno.test("short bigendian", () => {
   const b = new Uint8Array(10);
   short2bin(b, 2, 1024);
   t.assertEquals(bin2short(b, 2), 1024);
   t.assertEquals(bin2short(b, 0), 0);
   t.assertEquals(bin2short(b, 1), 1024 >> 8);
   t.assertEquals(bin2short(b, 3), 1024 & 0xff);
+});
+Deno.test("short littleendian", () => {
+  const b = new Uint8Array(10);
+  short2bin(b, 2, 1024, true);
+  t.assertEquals(bin2short(b, 2, true), 1024);
+  t.assertEquals(bin2short(b, 0, true), 0);
+  t.assertEquals(bin2short(b, 1, true), 1024 & 0xff);
+  t.assertEquals(bin2short(b, 3, true), 1024 >> 8);
+});
+Deno.test("int", () => {
+  const b = new Uint8Array(10);
+  i2bin(b, 2, 123456);
+  t.assertEquals(bin2i(b, 2), 123456);  
+  i2bin(b, 6, 123456, true);
+  t.assertEquals(bin2i(b, 6, true), 123456);
+  t.assert(b[2] != b[6]);
+  t.assert(b[2 + 3] == b[6]);
 });
